3985b0a8cefb986576f6cca693ed51e7b456974f
Aletheia <tdukazudon@gmail.com>
2018-12-04 19:45:05 +0900

    deckard:Rename and prepare
    Looking at 2020

--- device_deckard.mk
+++ device_deckard.mk
@@ -3,11 +3,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/sharp/is01/is01-vendor.mk)
+$(call inherit-product-if-exists, vendor/sharp/deckard/deckard-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/sharp/is01/overlay
+DEVICE_PACKAGE_OVERLAYS += device/sharp/deckard/overlay
 
-LOCAL_PATH := device/sharp/is01
+LOCAL_PATH := device/sharp/deckard
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
@@ -21,13 +21,17 @@ $(call inherit-product, build/target/product/full.mk)
 
 # Recovery
 PRODUCT_COPY_FILES += \
-    device/sharp/is01/tools/arm/strace:recovery/root/sbin/strace
+    device/sharp/deckard/tools/arm/strace:recovery/root/sbin/strace
 
 
 PRODUCT_COPY_FILES += \
-    device/sharp/is01/tools/arm/strace:system/xbin/strace
+    device/sharp/deckard/tools/arm/strace:system/xbin/strace
 
+#Proprietary files on dt by now,but it will migrate to vendor/sharp/deckard
+PRODUCT_COPY_FILES += \
+    device/sharp/deckard/libaudio.so:system/lib/libaudio.so
+		device/sharp/deckard/libloc_api.so:system/lib/libloc_api.so
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_is01
-PRODUCT_DEVICE := is01
+PRODUCT_NAME := full_deckard
+PRODUCT_DEVICE := deckard
