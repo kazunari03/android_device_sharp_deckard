0e811ed1b2ef6a5023271117a981deb141560c92
Aletheia <tdukazudon@gmail.com>
2018-12-05 18:03:48 +0900

    deckard:Add some files for JBonIS01
    Preparing device-tree files

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,17 +5,53 @@ USE_CAMERA_STUB := true
 
 TARGET_SPECIFIC_HEADER_PATH := device/sharp/deckard/include
 
+# Hardware configuration
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_QCOM_LIBS := true
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := false
+BOARD_HAVE_FM_RADIO := true
+BOARD_CAMERA_USE_GETBUFFERINFO := true
+BOARD_VENDOR_QCOM_AMSS_VERSION := 3200
+
+BOARD_VENDOR_USE_AKMD := akm8973
+
+# wlan
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION := VER_0_5_X
+# BOARD_WLAN_DEVICE := sdio
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/unifi_sdio.ko"
+WIFI_DRIVER_FW_STA_PATH := "/system/etc/firmware/unifi-sdio-1/a05/sta.xbv"
+# WIFI_DRIVER_MODULE_ARG := ""
+WIFI_DRIVER_MODULE_NAME := "unifi_sdio"
+
 TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := true
+TARGET_NO_RADIOIMAGE := true
+
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
 TARGET_BOARD_PLATFORM := qsd8k
 TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 ARCH_ARM_USE_NON_NEON_MEMCPY := true
-TARGET_BOOTLOADER_BOARD_NAME := is01
 
+# kernel configuration
 BOARD_KERNEL_CMDLINE := console=ttyMSM2,115200n8 androidboot.hardware=qcom
 BOARD_KERNEL_BASE := 0x20000000
 BOARD_KERNEL_PAGESIZE := 4096
+
+BOARD_PAGE_SIZE := 0x00001000
+BOARD_EGL_CFG := vendor/deckard/proprietary/lib/egl/egl.cfg
+
+#
+TARGET_HARDWARE_3D := false
+TARGET_BOARD_PLATFORM := qsd8k
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_BOOTLOADER_BOARD_NAME := deckard
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
@@ -24,8 +60,8 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_PREBUILT_KERNEL := device/sharp/deckard/kernel
-
+TARGET_SYSTEMIMAGES_USE_EXT2 := false
+TARGET_USERIMAGES_USE_EXT2 := false
 #BOARD_HAS_NO_SELECT_BUTTON := true
 # Use this flag if the board has a ext4 partition larger than 2gb
 #BOARD_HAS_LARGE_FILESYSTEM := true
--- device_deckard.mk
+++ device_deckard.mk
@@ -19,10 +19,48 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, build/target/product/full.mk)
 
+# Define Prelink
+PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/renax/prelink-linux-arm.map
+
+# Enabling Ring Tones
+include frameworks/base/data/sounds/OriginalAudio.mk
+
 # Recovery
 PRODUCT_COPY_FILES += \
     device/sharp/deckard/tools/arm/strace:recovery/root/sbin/strace
 
+# Publish that we support the live wallpaper feature and more hardware.
+		PRODUCT_COPY_FILES += \
+		frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+		frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+		frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+		frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+		frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+		frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+		frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+		frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+		frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+		frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml
+
+# Overrides
+		PRODUCT_BRAND := SHARP
+		PRODUCT_NAME := IS01
+		PRODUCT_DEVICE := IS01
+		PRODUCT_MODEL := IS01
+		PRODUCT_MANUFACTURER := SHARP
+
+# Additional build prop settings
+PRODUCT_PROPERTY_OVERRIDES += \
+		keyguard.no_require_sim=true \
+		ro.com.android.dateformat=MM-dd-yyyy \
+		ro.com.android.dataroaming=false \
+		ro.sf.lcd_density=240 \
+		ro.product.locale.language=ja \
+		ro.product.locale.region=JP
+
+#hardware revision
+PRODUCT_PROPERTY_OVERRIDES += \
+		ro.hw_version=2
 
 PRODUCT_COPY_FILES += \
     device/sharp/deckard/tools/arm/strace:system/xbin/strace
@@ -31,6 +69,20 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/sharp/deckard/libaudio.so:system/lib/libaudio.so
 		device/sharp/deckard/libloc_api.so:system/lib/libloc_api.so
+
+# WiFi configuration
+PRODUCT_PROPERTY_OVERRIDES += \
+		wifi.interface=wlan0 \
+		wifi.supplicant_scan_interval=15
+
+# The OpenGL ES API level that is natively supported by this device.
+# Enable cache on Google Maps
+PRODUCT_PROPERTY_OVERRIDES += \
+		ro.opengles.version=131072
+
+# This is a high density device with more memory, so larger vm heaps for it.
+PRODUCT_PROPERTY_OVERRIDES += \
+		dalvik.vm.heapsize=32m
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_deckard
